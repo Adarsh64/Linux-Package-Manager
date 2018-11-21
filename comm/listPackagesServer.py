d330f85385115d6edf6a1cde4f4fd9caf01efde7
Hrishikesh Suresh <blackleghubot@github.com>
2018-11-21 15:01:36 +0530

    Sending package list done

---
+++ comm/listPackagesServer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import socket
+import os
+import glob
+import time
+#creating socket
+packages = [f for f in glob.glob("Packages")]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connecting to client
+s.connect(('192.168.0.102', 2018))
+serversocket.listen(1)
+print("Connection successful!")
+s.send(packages)
+print("File sent!")
