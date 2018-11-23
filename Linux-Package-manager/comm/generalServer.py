505c7265afa74d3a060c119bc1f95d089f2d47c6
Adarsh64 <adarshnayak64@gmail.com>
2018-11-24 00:05:43 +0530

    Add files via upload

--- Linux-Package-manager/comm/generalServer.py
+++ Linux-Package-manager/comm/generalServer.py
@@ -7,18 +7,18 @@ import time
 
 #creating socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('10.0.0.2', 2018))
+s.bind(('192.168.43.15', 4006))
 s.listen(1)
 
 flag = True
 command = "online"
 print("Waiting for command!")
 while(flag):
-	time.sleep(2)
+	#time.sleep(2)
 	#accepting connection from client
 	clientsocket, addr = s.accept()
 	print("Message received!")
-	command = (clientsocket.recv(5000)).decode('utf-8')
+	command = clientsocket.recv(30).decode('utf-8')
 	if(command == "hello"):
 		print("Listing command executed!")
 		packages = os.listdir("../Packages")
@@ -27,13 +27,14 @@ while(flag):
 			info = os.stat(path)
 			#print(info.st_size)
 			#package_size = str(info.st_size)
-			package_size = os.path.getsize(path)
 			find_version = os.listdir(path)
+			package_size = str(os.path.getsize(path+"/"+str(find_version[0])))
+			
 			find_version = find_version[0].split(sep = "-")
 			version_number = str(find_version[-1])
-			version_number = version_number.replace(".tgz", "")
+			version_number = version_number.replace(".tar.gz", "")
 			print(i+","+package_size+","+version_number+";")
-			s.send((i+","+package_size+","+version_number+";").encode('utf-8'))
+			clientsocket.send((i+" " +package_size+" "+version_number+";").encode('utf-8'))
 		clientsocket.close()
 	elif("download " in command):
 		cmd = command.split(sep = " ")
@@ -41,22 +42,22 @@ while(flag):
 		#cmd[1] gives package name
 		for fzip in packages:
 			if(cmd[1] in fzip):
-				s.send(("../Packages/"+fzip).encode('utf-8'))
-		clientsocket.close()
-				break;
+				clientsocket.send((path+"/"+fzip).encode('utf-8'))
+				clientsocket.close()
+				break
 	elif("install" in command):
 		cmd = command.split(sep = " ")
 		packages = os.listdir("../Packages")
 		#cmd[1] gives package name
 		for fzip in packages:
 			if(cmd[1] in fzip):
-				s.send(("../install"+cmd[1]+".sh").encode('utf-8'))
-		clientsocket.close()
-				break;
+				clientsocket.send(("../install"+cmd[1]+".sh").encode('utf-8'))
+				clientsocket.close()
+				break
 	elif(command == "stop"):
 		clientsocket.close()
 		s.close()
-		break;
+		break
 	else:
 		clientsocket.close()
 		print("Command not defined!")
